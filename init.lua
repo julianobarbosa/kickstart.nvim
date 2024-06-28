9f1e584b0b57f012d61a3e9bc75705b7f9151e08
Juliano Barbosa <juliano.barbosa@hypera.com.br>
2024-06-28 11:15:00 -0300

    chore(nvim): added nvim-dap dependence

--- init.lua
+++ init.lua
@@ -282,7 +282,11 @@ require('lazy').setup({
   -- nvim-dap
   {
     'rcarriga/nvim-dap-ui',
-    dependencies = 'mfussenegger/nvim-dap',
+    dependencies = {
+      'mfussenegger/nvim-dap',
+      'nvim-neotest/nvim-nio',
+    },
+
     config = function()
       local dap = require 'dap'
       local dapui = require 'dapui'
